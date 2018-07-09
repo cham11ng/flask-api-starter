4ee9f180a7c82d2988070d46c96ce08c4d767e3b
Sagar Chamling <sagarchamling@lftechnology.com>
2018-07-09 16:32:44 +0545

    Fix lint

--- run.py
+++ run.py
@@ -1,9 +1,17 @@
+"""
+    Flask API starting point.
+"""
+
 from flask import Flask, jsonify
-app = Flask(__name__)
+
+
+app = Flask(__name__)  # pylint: disable=invalid-name
 
 
 @app.route('/')
 def home():
+    """Controller to handle / request."""
+
     return jsonify(name='Flask API', version='0.0.1')
 
 
